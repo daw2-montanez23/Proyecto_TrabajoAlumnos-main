609955eeb4bd729dbfb81fe0ad2d561897de645f
Francesc <montanezrosellfrancesc@fpllefia.com>
2023-06-09 11:02:46 +0200

    commit - acabado

--- Proyecto_TrabajoAlumnos-main/src/componentes/footer.js
+++ Proyecto_TrabajoAlumnos-main/src/componentes/footer.js
@@ -1,5 +1,5 @@
 export const footer = {
-    template: `<nav class="navbar navbar-expand-sm bg-light fixed-bottom text-center justify-content-center">
+    template: `<nav class="navbar navbar-expand-sm bg- fixed-bottom text-center justify-content-center">
         <p>Francesc Montañez Rosell</p>
     </nav>`
 }
